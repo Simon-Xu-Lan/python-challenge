097eed3b277fbbac76e361d46d7b6b69afe7611a
Simon Xu <xulan20907@gmail.com>
2021-01-05 14:27:05 -0700

    Added comments in PyPoll/main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,17 +6,12 @@ file_path = os.path.join("./", "Resources", "election_data.csv")
 
 # Create an empty dictionary to store election result
 election_result = {}
-# Create an empty set to store voters
-voter_sets = set()
-repeat_voters = set()
 
 # Initiate election_result
 election_result["total_votes"] = 0
 election_result["candidates"] = {}
 election_result["winner"] = {}
 winner_votes = 0
-
-
 
 with open(file_path, "r") as csv_file:
   csv_reader_obj = csv.reader(csv_file, delimiter=",")
@@ -28,13 +23,16 @@ with open(file_path, "r") as csv_file:
     # row[0]: Voter, row[2]: Candidate
     election_result["total_votes"] += 1
     if row[2] not in election_result["candidates"]:
-      # election_result["candidates"].add(row[2])
+      # If row[2] is not key of election_result["candidates"], add it as new key
+      # create a new object as value
       new_obj = {}
       new_obj["vote_count"] = 1
       election_result["candidates"][row[2]] = new_obj
     else:
+      # If row[2] is a key of election_result["candidates"], add 1 to its value
       election_result["candidates"][row[2]]["vote_count"] += 1 
 
+# Calculate percentage and winner
 total = election_result["total_votes"]
 winner = ""
 highest_votes = 0
@@ -48,8 +46,10 @@ for candidate in election_result["candidates"]:
 election_result["winner"] = winner
 
 # Create string for candidnate data
+# Using for loop inside f-string
 candidate_data_str = "\n".join(f"{key}: {value['percentage']} ({value['vote_count']})" for key, value in election_result['candidates'].items())
 
+# Create result string to print and save
 result = f"\
 Election Results\n\
 -------------------------\n\
