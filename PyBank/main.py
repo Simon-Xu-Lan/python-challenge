16615961416813921cea5c60565406143a716cc4
Simon Xu <xulan20907@gmail.com>
2021-01-05 14:17:24 -0700

    Final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,13 +14,16 @@ with open(file_path, "r") as csv_file:
 
   data_list = list(csv_reader_obj)
   total_month = len(data_list)
-  print(total_month)
+  # print(total_month)
 
   
   total_profit_loss = int(data_list[0][1])
   total_change = 0
   greatest_profit_increase = 0
   greatest_profit_decrease = 0
+  greatest_profit_increase_month = ""
+  greatest_profit_decrease_month = ""
+
   previous_month_profit = int(data_list[0][1])
 
   for i in range(1, total_month):
@@ -31,14 +34,24 @@ with open(file_path, "r") as csv_file:
     previous_month_profit = current_month_profit
     if greatest_profit_increase < change:
       greatest_profit_increase = change
+      greatest_profit_increase_month = data_list[i][0]
     if greatest_profit_decrease > current_month_profit:
       greatest_profit_decrease = change
-  
-  print(total_month)
-  print(total_profit_loss)
-  print(greatest_profit_decrease)
-  print(greatest_profit_increase)
-  # print(f'{total_month}')
-  print(f'{(total_change / (total_month - 1))}')
+      greatest_profit_decrease_month = data_list[i][0]
 
+  # print(total_profit_loss)
+  # print(total_change)
+result = f"\
+Financial Ananlysis\n\
+-------------------------------\n\
+Total Months: {total_month}\n\
+Total: ${total_profit_loss}\n\
+Average Change: ${round(total_change / (total_month - 1), 2)}\n\
+Greatest Increase in Profits: {greatest_profit_increase_month} (${greatest_profit_increase})\n\
+Greatest Decrease in Profits: {greatest_profit_decrease_month} (${greatest_profit_decrease})\n\
+"
+print(result)
+
+with open("PyBank_results.txt", "w") as file:
+  file.write(result)
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,33 +12,57 @@ repeat_voters = set()
 
 # Initiate election_result
 election_result["total_votes"] = 0
-election_result["candidates"] = set()
-election_result["results"] = {}
+election_result["candidates"] = {}
+election_result["winner"] = {}
+winner_votes = 0
+
+
 
 with open(file_path, "r") as csv_file:
   csv_reader_obj = csv.reader(csv_file, delimiter=",")
 
   # Retrieve the header and move the pointer to next line
   file_header = next(csv_reader_obj)
-  # print(file_header)
-
+  total_votes = 0
   for row in csv_reader_obj: 
-    if row[0] not in voter_sets:
-      voter_sets.add(row[0])
-      election_result["total_votes"] += 1
-      if row[2] not in election_result["candidates"]:
-        election_result["candidates"].add(row[2])
-        new_obj = {}
-        new_obj["name"] = row[2]
-        new_obj["vote_count"] = 1
-        election_result["results"][row[2]] = new_obj
-      else:
-        election_result["results"][row[2]]["vote_count"] += 1 
+    # row[0]: Voter, row[2]: Candidate
+    election_result["total_votes"] += 1
+    if row[2] not in election_result["candidates"]:
+      # election_result["candidates"].add(row[2])
+      new_obj = {}
+      new_obj["vote_count"] = 1
+      election_result["candidates"][row[2]] = new_obj
     else:
-      repeat_voters.add(row[0])
+      election_result["candidates"][row[2]]["vote_count"] += 1 
 
+total = election_result["total_votes"]
+winner = ""
+highest_votes = 0
+for candidate in election_result["candidates"]:
+  votes = election_result["candidates"][candidate]["vote_count"]
+  percentage = f"{round(votes / total * 100, 3)}00%"
+  election_result["candidates"][candidate]["percentage"] = percentage
+  if votes > highest_votes:
+    highest_votes = votes
+    winner = candidate
+election_result["winner"] = winner
 
-print(len(voter_sets))
-print(election_result)
+# Create string for candidnate data
+candidate_data_str = "\n".join(f"{key}: {value['percentage']} ({value['vote_count']})" for key, value in election_result['candidates'].items())
 
+result = f"\
+Election Results\n\
+-------------------------\n\
+Total Votes: {total}\n\
+-------------------------\n\
+{candidate_data_str}\n\
+-------------------------\n\
+Winner: {election_result['winner']}\n\
+-------------------------\n\
+"
 
+print(result)
+
+# Write the result to txt file
+with open("PyPoll_result.txt", "w") as file:
+  file.write(result)
